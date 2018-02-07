f4f03b8e58fd725a0fec9fdbdcf0db74b0d080b1
terrygonguet <terry@gonguet.com>
2018-02-07 18:05:05 +0000

    Functional camera

--- static/model/camera.js
+++ static/model/camera.js
@@ -2,13 +2,24 @@ class Camera extends createjs.EventDispatcher {
 
   constructor() {
     super();
+    this.center = $V([ 0,0 ]);
   }
 
   update(e) {
+    if (!game.player) return;
+    const playerPos = game.player.position.dup();
+    const dist = this.center.distanceFrom(playerPos);
+    if (dist > 0.8 * innerWidth) {
+      this.center = playerPos;
+    } else if (dist > 0.3 * innerHeight) {
+      var diff = playerPos.subtract(this.center);
+      this.center = this.center.add(diff.toUnitVector().x(dist - 0.3 * innerHeight));
+    }
     for (var child of game.children) {
       if (child.position) {
+        var screenPos = $V([ innerWidth/2, innerHeight/2 ]).subtract(this.center).add(child.position);
         child.set({
-          x: child.position.e(1), y: child.position.e(2)
+          x: screenPos.e(1), y: screenPos.e(2)
         });
       }
     }
